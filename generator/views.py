c3b884734cdf10d0474f091f7e6a89d83bac703c
Ryan <ryan@ryansolomon.io>
2021-05-31 15:41:13 -0600

    Link to password page

--- generator/views.py
+++ generator/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'generator/home.html', {'password': '222222'})
+
+
+def password(request):
+    return render(request, 'generator/password.html')
