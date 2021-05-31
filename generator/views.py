d2e000483e730c112795647b87b84d6edc9df760
Ryan <ryan@ryansolomon.io>
2021-05-31 15:32:25 -0600

    add password

--- generator/views.py
+++ generator/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'generator/home.html')
+    return render(request, 'generator/home.html', {'password': '222222'})
